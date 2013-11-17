a005813874414bca584708808a407c29f04ad95a
Roland.Germ <Roland.Germ@anecon.com>
2013-11-17 20:02:22 +0100

    Removed view2 from tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -28,18 +28,4 @@ describe('my app', function() {
 
   });
 
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
