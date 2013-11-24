914ffdc7d85427a57eaa7e3ee25d57be7559705d
Roland.Germ <Roland.Germ@anecon.com>
2013-11-25 00:42:39 +0100

    some more unit tests for the controllers

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,15 +2,53 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
-  beforeEach(module('vbTrainingApp.controllers'));
+describe('vbTrainingApp.controllers', function() {
+	beforeEach(module('vbTrainingApp.controllers'));
+	beforeEach(module('vbTrainingApp.services'));
 
+	describe('MainCtrl', function() {
+		var scope, ctrl, mock;
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+		mock = {
+			allPlayers : {}
+		};
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+		beforeEach(inject(function($rootScope, $controller) {
+			scope = $rootScope.$new();
+			ctrl = $controller('MainCtrl', {
+				$scope : scope,
+				PlayerService : mock
+			});
+		}));
+
+		it('should create initialize scope variables', function() {
+			expect(scope.parentobject.date).toBeNull();
+		});
+
+	});
+	
+	describe('SpielerListCtrl', function() {
+		var scope, ctrl, mock;
+
+		mock = {
+			allPlayers : [{
+				id : 1,
+				firstName : 'Test',
+				name : 'User',
+			}]
+		};
+
+		beforeEach(inject(function($rootScope, $controller) {
+			scope = $rootScope.$new();
+			ctrl = $controller('SpielerListCtrl', {
+				$scope : scope,
+				PlayerService : mock
+			});
+		}));
+
+		it('scope variable should be initialized to mock values', function() {
+			expect(scope.players.length).toEqual(1);
+		});
+
+	});
 });
